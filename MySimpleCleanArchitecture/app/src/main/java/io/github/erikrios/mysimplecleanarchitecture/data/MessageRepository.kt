80995d4e96e433a204db261a91bae5e6c23dd1ef
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-11 14:44:39 +0700

    Create MessageRepository.kt

---
+++ MySimpleCleanArchitecture/app/src/main/java/io/github/erikrios/mysimplecleanarchitecture/data/MessageRepository.kt
@@ -0,0 +1,7 @@
+package io.github.erikrios.mysimplecleanarchitecture.data
+
+import io.github.erikrios.mysimplecleanarchitecture.domain.IMessageRepository
+
+class MessageRepository(private val messageDataSource: IMessageDataSource) : IMessageRepository {
+    override fun getWelcomeMessage(name: String) = messageDataSource.getMessageFromSource(name)
+}
