22d9e66660b143a93ab1f8effdf40b712e57ee63
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-14 10:00:48 +0700

    Check the instance

--- MySimpleLogin/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
+++ MySimpleLogin/app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -4,20 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
-import org.koin.android.ext.android.inject
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
     @Inject
     lateinit var userRepository: UserRepository
+
+    @Inject
+    lateinit var userRepository2: UserRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (application as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        userRepository.checkInstance()
+        userRepository2.checkInstance()
 
         if (userRepository.isUserLogin()) {
             moveToHomeActivity()
