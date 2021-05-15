ce9783e43780fbde97648af7baee4f596c8504c1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-15 11:16:22 +0700

    Integrated the view binding

--- TourismAppModularization/maps/src/main/java/com/dicoding/tourism/maps/MapsActivity.kt
+++ TourismAppModularization/maps/src/main/java/com/dicoding/tourism/maps/MapsActivity.kt
@@ -1,11 +1,16 @@
 package com.dicoding.tourism.maps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.tourism.maps.databinding.ActivityMapsBinding
 
 class MapsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMapsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
