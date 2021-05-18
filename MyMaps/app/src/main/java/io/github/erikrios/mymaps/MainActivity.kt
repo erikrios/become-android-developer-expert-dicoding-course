fa8dbcbbb7c7a396b617bf9a88b5b9ef6a860757
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-19 01:17:58 +0700

    Add the route

--- MyMaps/app/src/main/java/io/github/erikrios/mymaps/MainActivity.kt
+++ MyMaps/app/src/main/java/io/github/erikrios/mymaps/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.api.directions.v5.models.DirectionsRoute
+import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
 import com.mapbox.mapboxsdk.location.LocationComponent
@@ -20,7 +23,11 @@ import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute
 import io.github.erikrios.mymaps.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +41,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var locationComponent: LocationComponent
     private lateinit var myLocation: LatLng
     private lateinit var permissionManager: PermissionsManager
+    private lateinit var navigationMapRoute: NavigationMapRoute
+    private var currentRoute: DirectionsRoute? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,6 +59,13 @@ class MainActivity : AppCompatActivity() {
                 style.addImage(
                     ICON_ID,
                     BitmapFactory.decodeResource(resources, R.drawable.mapbox_marker_icon_default)
+                )
+
+                navigationMapRoute = NavigationMapRoute(
+                    null,
+                    binding.mapView,
+                    mapBoxMap,
+                    R.style.NavigationMapRoute
                 )
 
                 showDicodingSpace()
@@ -134,8 +150,47 @@ class MainActivity : AppCompatActivity() {
                     .withIconImage(ICON_ID)
                     .withDraggable(true)
             )
+
+            val destination = Point.fromLngLat(point.longitude, point.latitude)
+            val origin = Point.fromLngLat(myLocation.longitude, myLocation.latitude)
+            requestRoute(origin, destination)
             true
         }
+    }
+
+    private fun requestRoute(origin: Point, destination: Point) {
+        navigationMapRoute.updateRouteVisibilityTo(false)
+        NavigationRoute.builder(this)
+            .accessToken(getString(R.string.mapbox_acess_token))
+            .origin(origin)
+            .destination(destination)
+            .build()
+            .getRoute(object : retrofit2.Callback<DirectionsResponse> {
+                override fun onResponse(
+                    call: Call<DirectionsResponse>,
+                    response: Response<DirectionsResponse>
+                ) {
+                    if (response.body() == null) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "No routes found, make sure you set the right user and access token.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        return
+                    } else if (response.body()?.routes()?.size == 0) {
+                        Toast.makeText(this@MainActivity, "No routes found", Toast.LENGTH_SHORT)
+                            .show()
+                        return
+                    }
+
+                    currentRoute = response.body()?.routes()?.get(0)
+                    navigationMapRoute.addRoute(currentRoute)
+                }
+
+                override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "Error: $t", Toast.LENGTH_SHORT).show()
+                }
+            })
     }
 
     override fun onStart() {
