7d6e517639c48253eeeba7c056d0c38cdfbad3b5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-18 22:50:00 +0700

    Create MyApplication.kt

---
+++ MyReportApp/app/src/main/java/io/github/erikrios/myreportapp/MyApplication.kt
@@ -0,0 +1,14 @@
+package io.github.erikrios.myreportapp
+
+import android.app.Application
+import timber.log.Timber
+
+open class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
