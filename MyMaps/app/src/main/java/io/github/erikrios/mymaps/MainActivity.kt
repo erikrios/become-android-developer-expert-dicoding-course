858ccba81871a7dcd1612698483437ea654c677f
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-19 00:22:29 +0700

    Implements the MapBox

--- MyMaps/app/src/main/java/io/github/erikrios/mymaps/MainActivity.kt
+++ MyMaps/app/src/main/java/io/github/erikrios/mymaps/MainActivity.kt
@@ -2,15 +2,60 @@ package io.github.erikrios.mymaps
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
 import io.github.erikrios.mymaps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mapBoxMap: MapboxMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync { mapBoxMap ->
+            this.mapBoxMap = mapBoxMap
+            mapBoxMap.setStyle(Style.MAPBOX_STREETS) { style ->
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
     }
 }
