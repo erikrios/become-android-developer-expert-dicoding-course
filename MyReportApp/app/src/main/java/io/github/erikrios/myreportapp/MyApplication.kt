0c46d094e6a758558f1098eda4b399e74655434a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-18 22:57:56 +0700

    Add release log

--- MyReportApp/app/src/main/java/io/github/erikrios/myreportapp/MyApplication.kt
+++ MyReportApp/app/src/main/java/io/github/erikrios/myreportapp/MyApplication.kt
@@ -1,6 +1,7 @@
 package io.github.erikrios.myreportapp
 
 import android.app.Application
+import io.github.erikrios.myreportapp.utils.ReleaseTree
 import timber.log.Timber
 
 open class MyApplication : Application() {
@@ -9,6 +10,8 @@ open class MyApplication : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(ReleaseTree())
         }
     }
 }
