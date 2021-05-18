3c332ff62f7f636212da6e616b3c544417422c53
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-05-19 01:03:48 +0700

    Get the user current location

--- MyMaps/app/src/main/java/io/github/erikrios/mymaps/MainActivity.kt
+++ MyMaps/app/src/main/java/io/github/erikrios/mymaps/MainActivity.kt
@@ -1,10 +1,21 @@
 package io.github.erikrios.mymaps
 
+import android.annotation.SuppressLint
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.view.animation.BounceInterpolator
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
@@ -20,6 +31,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapBoxMap: MapboxMap
     private lateinit var symbolManager: SymbolManager
+    private lateinit var locationComponent: LocationComponent
+    private lateinit var myLocation: LatLng
+    private lateinit var permissionManager: PermissionsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +53,7 @@ class MainActivity : AppCompatActivity() {
                 )
 
                 showDicodingSpace()
+                showMyLocation(style)
             }
         }
     }
@@ -60,6 +75,52 @@ class MainActivity : AppCompatActivity() {
         )
 
         mapBoxMap.moveCamera(CameraUpdateFactory.newLatLngZoom(dicodingSpace, 8.0))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showMyLocation(style: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponentOptions = LocationComponentOptions.builder(this)
+                .pulseEnabled(true)
+                .pulseColor(Color.BLUE)
+                .pulseAlpha(.4f)
+                .pulseInterpolator(BounceInterpolator())
+                .build()
+            val locationComponentActivationOptions = LocationComponentActivationOptions
+                .builder(this, style)
+                .locationComponentOptions(locationComponentOptions)
+                .build()
+            locationComponent = mapBoxMap.locationComponent
+            locationComponent.activateLocationComponent(locationComponentActivationOptions)
+            locationComponent.isLocationComponentEnabled = true
+            locationComponent.cameraMode = CameraMode.TRACKING
+            locationComponent.renderMode = RenderMode.COMPASS
+
+            myLocation = LatLng(
+                locationComponent.lastKnownLocation?.latitude as Double,
+                locationComponent.lastKnownLocation?.longitude as Double
+            )
+            mapBoxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 12.0))
+        } else {
+            permissionManager = PermissionsManager(object : PermissionsListener {
+                override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Anda harus mengizinkan location permission untuk menggunakan aplikasi ini",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onPermissionResult(granted: Boolean) {
+                    if (granted) {
+                        mapBoxMap.getStyle { style -> showMyLocation(style) }
+                    } else {
+                        finish()
+                    }
+                }
+            })
+            permissionManager.requestLocationPermissions(this)
+        }
     }
 
     override fun onStart() {
